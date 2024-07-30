c24050986624fe7ecf7860dbbb4c81e858056e36
Daniel Cassell <danielcassell@Daniels-MacBook-Pro.local>
2024-07-30 10:30:33 +0100

    Add sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+// next-sitemap.config.js
+
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://avonvale-bowls-club.vercel.app/',
+    generateRobotsTxt: true,             // (optional) Generate a robots.txt file
+    // Additional options
+  }
+  
